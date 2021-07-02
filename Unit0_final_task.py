e3f952608064e2bf102494ec96e8d31cf3898518
Rostislav <rostislav.lanskoj@yandex.ru>
2021-07-02 05:42:47 +0300

    Unit0_final_task: Итоговый вариант задания.

--- Unit0_final_task.py
+++ Unit0_final_task.py
@@ -1,19 +1,10 @@
-playing_field = [
-    [" ", '0', '1', '2'],
-    ["0", '-', '-', '-'],
-    ["1", '-', '-', '-'],
-    ["2", '-', '-', '-'],
-]
-
-move_number = 1
-winner = None
+tic = 1
+tac = -1
 
 
-def show_field():
-    for i in range(4):
-        for j in range(4):
-            print(playing_field[j][i], end=" ")
-        print()
+def generate_playing_field(n):
+    playing_field_g = [[0] * n for _ in range(n)]
+    return playing_field_g
 
 
 def start_game():
@@ -26,42 +17,78 @@ def start_game():
         check_start = input('Для запуска игры введите Start \n')
         check_start = check_start.lower()
 
-    show_field()
+
+start_game()
+field_size = int(input("Введите размер поля \n"))
+playing_field = generate_playing_field(field_size)
 
 
-def do_move():
+def show_field(n):
+    print("  ", end="")
+    print(*range(1, n+1))
+    for i in range(n):
+        print(i+1, end=" ")
+        for j in range(n):
+            cur = playing_field[j][i]
+            print(("x" if cur == tic else "o")if abs(cur) == 1 else "-", end=" ")
+        print()
+
+
+show_field(field_size)
+move_number = 1
+winner = None
+
+
+def do_move(n, p_f):
     global move_number
-    enter_value_split = input(('Крестики' if move_number % 2 else "Нолики") + ' cделайте ход \n').split()
+    while True:
+        stop = 1
+        enter_value = input(('Крестики' if move_number % 2 else "Нолики") + ' cделайте ход \n')
+        enter_value_split = enter_value.split()
+        for i in enter_value_split:
+            if not i.isdigit():
+                stop = 0
+            elif int(i) <= 0 or int(i) > int(n):
+                stop = 0
+        if len(enter_value_split) != 2:
+            stop = 0
+
+        if stop and p_f[int(enter_value_split[0])-1][int(enter_value_split[1])-1] == 0:
+            break
+
     return enter_value_split[0], enter_value_split[1]
 
 
-def end_check():
-    global winner
-    for i in range(1, 4):
-        if playing_field[1][i] == playing_field[2][i] == playing_field[3][i] and (playing_field[1][i] != "-"):
-            winner = playing_field[1][i]
-        elif playing_field[i][1] == playing_field[i][2] == playing_field[i][3] and (playing_field[i][1] != "-"):
-            winner = playing_field[i][1]
-        elif playing_field[1][1] == playing_field[2][2] == playing_field[3][3] and (playing_field[1][1] != "-"):
-            winner = playing_field[1][1]
-        elif playing_field[3][1] == playing_field[2][2] == playing_field[1][3] and (playing_field[3][1] != "-"):
-            winner = playing_field[3][1]
+def end_check(n):
+    sum_f_di = 0
+    sum_s_di = 0
+    for i in range(n):
+        sum_f_di += playing_field[i][i]
+        sum_s_di += playing_field[i][n-1-i]
+    if abs(sum_s_di) == n:
+        return sum_s_di
 
+    if abs(sum_f_di) == n:
+        return sum_f_di
 
+    for i in range(n):
+        if abs(sum(playing_field[i])) == n:
+            return sum(playing_field[i])
+        elif abs(sum(list(zip(*playing_field))[i])) == n:
+            return sum(list(zip(*playing_field))[i])
+    return False
 
-start_game()
 
 while True:
-
-    x, y = map(int,(do_move()))
-    playing_field[x+1][y+1] = "x" if move_number % 2 else "o"
+    x, y = map(int, do_move(field_size, playing_field))
+    playing_field[x-1][y-1] = tic if move_number % 2 else tac
     move_number += 1
-    show_field()
-    end_check()
-    if winner or move_number == 10:
+    show_field(field_size)
+    winner = end_check(field_size)
+    if winner or move_number == field_size**2 + 1:
         break
 
-if move_number != 10 or winner:
-    print("Игра окончена, победили " + ("крестики" if winner == "x" else "нолики"))
+if move_number != field_size**2 + 1 or winner:
+    print("Игра окончена, победили " + ("крестики" if winner == field_size else "нолики"))
 else:
     print("Игра окончена, ничья")
